458d8da1e9b3a3a65f6a7a8a49204888cdf0ecf9
majerto4 <majerto4@fel.cvut.cz>
2022-03-20 15:16:46 +0100

    gui class

--- src/main/java/cz/cvut/fel/pjv/Main.java
+++ src/main/java/cz/cvut/fel/pjv/Main.java
@@ -1,7 +1,12 @@
 package cz.cvut.fel.pjv;
 
+import cz.cvut.fel.pjv.*;
+import cz.cvut.fel.pjv.gui.StartMenu;
+
+
 public class Main {
     public static void main(String[] args){
-
+        StartMenu menu = new StartMenu();
+        menu.launch();
     }
 }
