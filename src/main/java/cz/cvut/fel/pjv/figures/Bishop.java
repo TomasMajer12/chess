893a136b1178a5d106cff05a756ef0f2695ed8f9
majerto4 <majerto4@fel.cvut.cz>
2022-04-06 18:44:52 +0200

    figures constructors

--- src/main/java/cz/cvut/fel/pjv/figures/Bishop.java
+++ src/main/java/cz/cvut/fel/pjv/figures/Bishop.java
@@ -1,4 +1,10 @@
 package cz.cvut.fel.pjv.figures;
 
+import cz.cvut.fel.pjv.game.ChessField;
+import javafx.scene.paint.Color;
+
 public class Bishop extends Figure{
+    public Bishop(Color color, String name, ChessField field) {
+        super(color, name, field);
+    }
 }
